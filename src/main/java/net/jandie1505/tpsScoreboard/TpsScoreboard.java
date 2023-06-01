6ed7e94b6ca492e97c045213c5e000e6ecb4b34e
jandie1505 <34031551+jandie1505@users.noreply.github.com>
2023-06-01 13:00:25 +0200

    complete rework

--- src/main/java/net/jandie1505/tpsScoreboard/TpsScoreboard.java
+++ src/main/java/net/jandie1505/tpsScoreboard/TpsScoreboard.java
@@ -3,62 +3,59 @@ package net.jandie1505.tpsScoreboard;
 import dev.jorel.commandapi.CommandAPI;
 import dev.jorel.commandapi.CommandAPIBukkitConfig;
 import dev.jorel.commandapi.CommandAPICommand;
-import dev.jorel.commandapi.CommandAPIConfig;
-import net.jandie1505.tpsScoreboard.config.Config;
-import net.jandie1505.tpsScoreboard.tasks.Lag;
-import net.jandie1505.tpsScoreboard.tasks.tpsScoreboard;
-import org.bukkit.Bukkit;
-import org.bukkit.command.ConsoleCommandSender;
-import org.bukkit.entity.Player;
+import net.jandie1505.configmanager.ConfigManager;
+import net.minecraft.server.MinecraftServer;
+import org.bukkit.craftbukkit.v1_19_R3.CraftServer;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.json.JSONObject;
 
 public class TpsScoreboard extends JavaPlugin {
-    private static TpsScoreboard plugin;
+    private ConfigManager configManager;
 
     public void onLoad(){
-        plugin = this;
-        Config.load();
+        this.configManager = new ConfigManager(this.getDefaultConfig(), false, this.getDataFolder(), "config.json");
         CommandAPI.onLoad(new CommandAPIBukkitConfig(this));
 
         CommandAPICommand getTpsCommand = new CommandAPICommand("gettps")
                 .withPermission("tpsscoreboard.gettps")
-                .executesPlayer((sender, args) -> {
-                    Player p = (Player) sender;
-                    p.sendMessage("§eCurrent TPS: " + Lag.getTPS());
-                    return (int) Lag.getTPS();
-                })
-                .executesConsole((sender, args) -> {
-                    sender.sendMessage("§eCurrent TPS: " + Lag.getTPS());
-                    return (int) Lag.getTPS();
-                })
-                .executesProxy((proxy, args) -> {
-                    return (int) Lag.getTPS();
+                .executes((sender, args) -> {
+
+                    MinecraftServer minecraftServer = ((CraftServer) this.getServer()).getServer();
+                    double tps = minecraftServer.recentTps[0];
+
+                    if (tps > 20.0) {
+                        tps = 20.0;
+                    }
+
+                    if (!this.configManager.getConfig().optBoolean("silentMode", false)) {
+                        sender.sendMessage("§eCurrent TPS: §b" + tps);
+                    }
+
+                    return (int) Math.round(tps);
+
                 });
 
         getTpsCommand.register();
 
-        System.out.println("[tpsScoreboard] tpsScoreboard was loaded");
+        this.getLogger().info("tpsScoreboard was loaded");
     }
 
-    public void onEnable(){
-        Config.load();
+    public void onEnable() {
+        this.configManager.reloadConfig();
         CommandAPI.onEnable();
 
-        Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(this, new Lag(), 100L, 1L);
-        if(Config.writeTpsIntoScoreboard){
-            try {
-                tpsScoreboard.init();
-                Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(this, new tpsScoreboard(), 0, Config.tickInterval);
-            } catch (Exception e){
-                e.printStackTrace();
-                System.out.println("[tpsScoreboard] An error occured with scheduling the task which writes the current TPS into the scoreboard. Please check the config.");
-            }
-        }
-
-        System.out.println("[tpsScoreboard] tpsScoreboard was enabled");
+        this.getLogger().info("tpsScoreboard was enabled");
     }
 
-    public static TpsScoreboard getPlugin(){
-        return plugin;
+    public ConfigManager getConfigManager() {
+        return this.configManager;
+    }
+
+    private JSONObject getDefaultConfig() {
+        JSONObject config = new JSONObject();
+
+        config.put("silentMode", false);
+
+        return config;
     }
 }
